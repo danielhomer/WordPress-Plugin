8eeb2b9fee6acab1dedbc8eb6b63bc406abb684b
Daniel Homer <danielhomer@me.com>
2018-11-21 12:51:11 +0000

    Don't serve stale on certain URLs

--- src/wp-purges.php
+++ src/wp-purges.php
@@ -3,96 +3,93 @@
 /**
  * Singleton for registering default WP purges.
  */
-class Purgely_Purges
-{
-    /**
-     * The one instance of Purgely_Purges.
-     *
-     * @var Purgely_Purges
-     */
-    private static $instance;
+class Purgely_Purges {
 
-    /**
-     * Instantiate or return the one Purgely_Purges instance.
-     *
-     * @return Purgely_Purges
-     */
-    public static function instance()
-    {
-        if (is_null(self::$instance)) {
-            self::$instance = new self();
-        }
+	/**
+	 * The one instance of Purgely_Purges.
+	 *
+	 * @var Purgely_Purges
+	 */
+	private static $instance;
 
-        return self::$instance;
-    }
+	/**
+	 * Instantiate or return the one Purgely_Purges instance.
+	 *
+	 * @return Purgely_Purges
+	 */
+	public static function instance() {
+		if ( is_null( self::$instance ) ) {
+			self::$instance = new self();
+		}
 
-    /**
-     * Initiate actions.
-     */
-    public function __construct()
-    {
-        foreach ($this->_purge_actions() as $action) {
-            add_action($action, array($this, 'purge'), 10, 1);
-        }
-        
-        add_action( 'shutdown', [ $this, 'maybe_send_purge' ], 100 ); // Purge requests should be the last thing WP does
-    }
+		return self::$instance;
+	}
 
-    public function maybe_send_purge() {
-    	$collections = apply_filters( 'fastly_purge_collections', [] );
+	/**
+	 * Initiate actions.
+	 */
+	public function __construct() {
+		foreach ( $this->_purge_actions() as $action ) {
+			add_action( $action, [ $this, 'purge' ], 10, 1 );
+		}
 
-    	if ( ( current_user_can( 'publish_posts' ) && ! empty( $collections ) ) || ( defined( 'WP_CLI' ) && WP_CLI ) ) {
-		    $purgely = new Purgely_Purge();
-		    foreach ($collections as $collection) {
-			    $purgely->purge('key-collection', $collection);
-		    }
-	    }
-    }
+		add_action( 'shutdown', [ $this, 'maybe_send_purge' ], 100 ); // Purge requests should be the last thing WP does
+	}
 
-    /**
-     * Callback for post changing events to purge keys.
-     *
-     * @param  int $post_id Post ID.
-     * @return void
-     */
-    public function purge($post_id)
-    {
+	public function maybe_send_purge() {
+		$collections = apply_filters( 'fastly_purge_collections', [] );
 
-        if (!in_array(get_post_status($post_id), array('publish', 'trash', 'future', 'draft'))) {
-            return;
-        }
+		if ( ( current_user_can( 'publish_posts' ) && ! empty( $collections ) ) || ( defined( 'WP_CLI' ) && WP_CLI ) ) {
+			$purgely = new Purgely_Purge();
+			foreach ( $collections as $collection ) {
+				$purgely->purge( 'key-collection', $collection );
+			}
+		}
+	}
 
-        // Check credentials
-        $fastly_hostname = Purgely_Settings::get_setting('fastly_api_hostname');
-        $fastly_service_id = Purgely_Settings::get_setting('fastly_service_id');
-        $fastly_api_key = Purgely_Settings::get_setting('fastly_api_key');
-        $test = test_fastly_api_connection($fastly_hostname, $fastly_service_id, $fastly_api_key);
-        if (!$test['status']) {
-            return;
-        }
+	/**
+	 * Callback for post changing events to purge keys.
+	 *
+	 * @param  int $post_id Post ID.
+	 *
+	 * @return void
+	 */
+	public function purge( $post_id ) {
 
-        $related_collection_object = new Purgely_Related_Surrogate_Keys($post_id);
-        $collections = $related_collection_object->locate_all();
+		if ( ! in_array( get_post_status( $post_id ), [ 'publish', 'trash', 'future', 'draft' ] ) ) {
+			return;
+		}
 
-        add_filter( 'fastly_purge_collections', function( $c ) use ( $collections ) {
-        	return array_merge( $c, $collections );
-        }, 10, 1 );
-    }
+		// Check credentials
+		$fastly_hostname = Purgely_Settings::get_setting( 'fastly_api_hostname' );
+		$fastly_service_id = Purgely_Settings::get_setting( 'fastly_service_id' );
+		$fastly_api_key = Purgely_Settings::get_setting( 'fastly_api_key' );
+		$test = test_fastly_api_connection( $fastly_hostname, $fastly_service_id, $fastly_api_key );
+		if ( ! $test['status'] ) {
+			return;
+		}
 
-    /**
-     * A list of actions to purge URLs.
-     *
-     * @return array    List of actions.
-     */
-    private function _purge_actions()
-    {
-        return array(
-            'save_post',
-            'deleted_post',
-            'trashed_post',
-            'delete_attachment',
-        );
-    }
+		$related_collection_object = new Purgely_Related_Surrogate_Keys( $post_id );
+		$collections = $related_collection_object->locate_all();
+
+		add_filter( 'fastly_purge_collections', function ( $c ) use ( $collections ) {
+			return array_merge( $c, $collections );
+		}, 10, 1 );
+	}
+
+	/**
+	 * A list of actions to purge URLs.
+	 *
+	 * @return array    List of actions.
+	 */
+	private function _purge_actions() {
+		return [
+			'save_post',
+			'deleted_post',
+			'trashed_post',
+			'delete_attachment',
+		];
+	}
 }
 
 /**
@@ -100,9 +97,8 @@ class Purgely_Purges
  *
  * @return Purgely_Purges
  */
-function get_purgely_purges_instance()
-{
-    return Purgely_Purges::instance();
+function get_purgely_purges_instance() {
+	return Purgely_Purges::instance();
 }
 
 get_purgely_purges_instance();
